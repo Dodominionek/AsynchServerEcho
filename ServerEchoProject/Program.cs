b185b92c55137a9b819f29692a80a9b9e266d1a0
Dodominionek <dt532@interia.pl>
2020-10-29 19:51:03 +0100

    Add

---
+++ ServerEchoProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using ServerEchoLibrary;
+
+namespace ServerEchoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            AsyncServerEcho server = new AsyncServerEcho(IPAddress.Parse("127.0.0.1"), 10000);
+            server.Start();
+        }
+    }
+}
